39b2e5e61420f4018300e8311b4674c8f6b6fa7f
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2022-04-10 23:11:55 +0000

    Gave names to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   #get("/", controller: "movies", action: "index")
 
-  get "/" => "movies#index"
+  root "movies#index"
 
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new"  => "movies#new"
+  post "/movies" => "movies#create", as: :movies #movies_url and movies_path
+  get "/movies/new"  => "movies#new",  as: :new_movie #new_movie_url
           
   # READ
-  get "/movies"  => "movies#index"
-  get "/movies/:id"  => "movies#show"
+  get "/movies"  => "movies#index" 
+  get "/movies/:id"  => "movies#show", as: :movie #movies_path() expects and argument to populate :id
   
   # UPDATE
   patch "/movies/:id"  => "movies#update"
-  get "/movies/:id/edit"  => "movies#edit"
+  get "/movies/:id/edit"  => "movies#edit", as: :edit_movie
   
   # DELETE
   delete "/movies/:id"  => "movies#destroy"
